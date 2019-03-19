a8d6dc99fadaea36e5fdeafd17decdd49a5df9bc
Sahariar Alam Khandoker <sahariar.khandoker@bjitgroup.com>
2019-03-19 14:59:35 +0600

    homePage added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
