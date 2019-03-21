4b85650968c9d15e34a05bc9c8d1faf366d14018
arafatul.islam <arafatul.islam@bjitgroup.com>
2019-03-21 09:21:04 +0600

    search by disease and symptom done

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { $ } from 'protractor';
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+selector: 'app-search-page',
+templateUrl: './search-page.component.html',
+styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
+
+data = [];
+configUrl = "";
+onClickSubmit(data) {
+    this.htmlVariable ="";
+    console.log(data.dissym);
+    var disname;
+    if(data.dissym== 0){
+        this.configUrl = "http://192.168.50.137:8080/api/v1/search/by/symptoms/";
+        disname={
+            "symptoms" : data.disease.split(',') 
+        };
+    }
+    else{
+        this.configUrl = "http://192.168.50.137:8080/api/v1/search/by/disease/";
+        disname={
+            "name" : data.disease
+        };
+    }
+        this.http.post(this.configUrl,
+            disname).subscribe(
+        (val) => {
+            console.log("POST call successful value returned in body", 
+            val.hits.hits.length);
+            this.data = val.hits.hits;
+            if(val.hits.hits.length>0){
+                console.log("POST call successful value returned in body", 
+            val);
+            console.log("POST call successful value returned in body", 
+            val.hits.hits[0]);
+            }
+            else{
+                this.htmlVariable = "<span>No Data Found</span>";
+            }
+        },
+        response => {
+            console.log("POST call in error", response);
+        },
+        () => {
+            console.log("The POST observable is now completed.");
+        });
+// return this.http.get(this.configUrl);
+}
 
 }
